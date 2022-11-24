a6050a8629f409d3bf3c7c3f1b85b6f6d6c79fdb
MathMattar <mathmattar@hotmail.com>
2022-11-23 22:40:08 -0300

    Update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
+var btn = document.querySelector('.action-btn');
+var answer = document.querySelectorAll('.toggle');
 
+btn.addEventListener("click", ()=>{
+
+    answer.forEach(el => {    
+    el.addEventListener("click", function (e) {
+        
+        e.preventDefault ();
+        
+
+    })
+    })
+});
+
+
+
+
+
+/*  var answer = document.getElementById('toggle#2');
+    var answer = document.getElementById('toggle#3');
+    var answer = document.getElementById('toggle#4');
+    var answer = document.getElementById('toggle#5'); */
+
+
+    /* classList.toggle("hide"); */
