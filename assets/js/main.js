36c9e24cf9367d740e392a6a063ca2bd52742497
MathMattar <mathmattar@hotmail.com>
2022-11-25 15:48:57 -0300

    Update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,20 @@
-var question = document.getElementsById("question");
+var btn = document.getElementsById("question");
 var i;
 
-for (i = 0; i < question.length; i++) {
-    question[i].addEventListener("click", function () {
+for (i = 0; i < btn.length; i++) {
+
+    pergunta[i].addEventListener("click", function () {
+
         this.classList.toggle("active");
+
         var answer = this.nextElementSibling;
+
         if (answer.style.display === "block") {
+
             answer.style.display = "none";
+
         } else {
+            
             answer.style.display = "block";
         }
     })
