18788b641a87c42483ec30f96370920b0a1c69f8
MathMattar <mathmattar@hotmail.com>
2022-11-26 16:03:21 -0300

    Update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-    const activeBtns = document.querySelectorAll('.question');
-    for (activeBtn of activeBtns) {
-        activeBtn.onclick = exibirResposta;
-    }
-    function exibirResposta(e) {
-        let resposta = e.target.parentNode.nextElementSibling;
-        resposta.classList.toggle('show');
+const activeBtns = document.querySelectorAll('.question');
+for (activeBtn of activeBtns) {
+    activeBtn.onclick = exibirResposta;
+}
+function exibirResposta(e) {
+    let resposta = e.target.parentNode.nextElementSibling;
+    resposta.classList.toggle('show');
 
-    }
+}
